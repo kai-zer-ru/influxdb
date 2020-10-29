ad55c0cd92396634c219968a57a9470760978b1c
kaizer <kaizer@kai-zer.ru>
2020-10-29 10:47:05 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package influxdb
+
+import (
+	"context"
+	"time"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+)
+
+// InfluxDB struct
+type InfluxDB struct {
+	HostPort         string
+	isConnected      bool
+	client           influxdb2.Client
+	writeAPI         api.WriteAPIBlocking
+	MainDatabaseName string
+}
+
+// Connect to influxdb
+func (i *InfluxDB) Connect() {
+	if !i.isConnected {
+		i.client = influxdb2.NewClient("http://"+i.HostPort, "")
+		i.writeAPI = i.client.WriteAPIBlocking("my-org", "my-bucket")
+		i.isConnected = true
+	}
+}
+
+// SendData to influxdb
+func (i *InfluxDB) SendData(daemonName, pointName string, value interface{}) {
+	if !i.isConnected {
+		return
+	}
+	p := influxdb2.NewPoint(i.MainDatabaseName,
+		map[string]string{daemonName: pointName},
+		map[string]interface{}{"value": value},
+		time.Now(),
+	)
+	i.writeAPI.WritePoint(context.Background(), p)
+}
+
+// Close influxdb
+func (i *InfluxDB) Close() {
+	if !i.isConnected {
+		return
+	}
+	i.client.Close()
+	i.isConnected = false
+}
